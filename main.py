9d450649da4f93241928ff9c0e59edd1763889a0
nopchok <30373891+nopchok@users.noreply.github.com>
2021-09-07 14:40:45 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+import os
+import json
+from threading import Thread
+
+
+from replit.database import Database
+
+print(os.environ["REPL_ID"])
+db = Database(os.environ["REPLIT_DB_URL"])
+print(db)
+
+def main():
+
+    app = Flask(__name__)
+    CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
+    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+    @app.route('/', methods=['GET'])
+    def home():
+        return {}
+
+    @app.route('/update', methods=['POST'])
+    def updatedata():
+        post_data = json.loads(request.data)
+        d = list(post_data.keys())
+        d.sort()
+        if d == ['data', 'key']:
+          key = post_data['key']
+          data = post_data['data']
+          db[key] = data
+          return {'result': 'ok'}
+        return {}
+
+    @app.route('/get', methods=['GET'])
+    @cross_origin()
+    def getdata():
+      try:
+        keys = request.args.get('keys').lower()
+        keys = keys.split('|')
+        res = {}
+        for _ in keys:
+          if db.get(_):
+            res[_] = dict(db[_])
+
+        return {"result": res}
+      except:
+        return {}
+
+    def run():
+        app.run(host='0.0.0.0')
+
+    apiserver = Thread(target=run)
+    apiserver.start()
+
+
+if __name__ == '__main__':
+    main()
